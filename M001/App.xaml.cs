a29b5a85aa84cfce8194107e8cb2acb69d587752
ppedvLukasK <lukask@ppedv.de>
2024-10-28 10:48:21 +0100

    M001, M002

---
+++ M001/App.xaml.cs
@@ -0,0 +1,16 @@
+﻿using System.Windows;
+
+namespace M001;
+
+public partial class App : Application
+{
+	private void Application_Startup(object sender, StartupEventArgs e)
+	{
+
+	}
+
+	private void Application_Exit(object sender, ExitEventArgs e)
+	{
+
+	}
+}
---
+++ M002/MainWindow.xaml.cs
@@ -0,0 +1,33 @@
+﻿using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace M002;
+
+public partial class MainWindow : Window
+{
+	public MainWindow()
+	{
+		InitializeComponent();
+
+		CB.ItemsSource = new int[] { 1, 2, 3, 4, 5 };
+		LB.ItemsSource = new int[] { 1, 2, 3, 4, 5 };
+	}
+
+	private void Button_Click(object sender, RoutedEventArgs e)
+	{
+		Output.Text = Input.Text;
+	}
+
+	private void Slider_ValueChanged(object sender, RoutedPropertyChangedEventArgs<double> e)
+	{
+		SliderValue.Text = e.NewValue.ToString();
+	}
+}
