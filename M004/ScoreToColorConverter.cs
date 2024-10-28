b65e7031c57f12a7332fdb742606024078437ca9
ppedvLukasK <lukask@ppedv.de>
2024-10-28 15:25:51 +0100

    M004

---
+++ M004/ScoreToColorConverter.cs
@@ -0,0 +1,41 @@
+﻿using System.Globalization;
+using System.Windows.Data;
+using System.Windows.Media;
+
+namespace M004;
+
+public class ScoreToColorConverter : IValueConverter
+{
+	/// <summary>
+	/// Quelle -> Ziel
+	/// Slider -> TextBlock
+	/// Value -> Background
+	/// double -> Brush
+	/// </summary>
+	public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+	{
+		double d = (double) value;
+		switch (d)
+		{
+			case >= 90: return new SolidColorBrush(Colors.Green);
+			case >= 80: return new SolidColorBrush(Colors.LightGreen);
+			case >= 70: return new SolidColorBrush(Colors.Yellow);
+			case >= 60: return new SolidColorBrush(Colors.Orange);
+			case >= 50: return new SolidColorBrush(Colors.DarkOrange);
+			default: return new SolidColorBrush(Colors.Red);
+		}
+	}
+
+	/// <summary>
+	/// Ziel -> Quelle
+	/// TextBlock -> Slider
+	/// Text -> Value
+	/// string -> double
+	/// 
+	/// (wird hier nicht benötigt)
+	/// </summary>
+	public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+	{
+		throw new NotImplementedException();
+	}
+}
---
+++ M004/ScoreToGradeConverter.cs
@@ -0,0 +1,40 @@
+﻿using System.Globalization;
+using System.Windows.Data;
+
+namespace M004;
+
+public class ScoreToGradeConverter : IValueConverter
+{
+	/// <summary>
+	/// Quelle -> Ziel
+	/// Slider -> TextBlock
+	/// Value -> Text
+	/// double -> string
+	/// </summary>
+	public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+	{
+		double d = (double) value;
+		switch (d)
+		{
+			case >= 90: return "Sehr Gut";
+			case >= 80: return "Gut";
+			case >= 70: return "Befriedigend";
+			case >= 60: return "Ausreichend";
+			case >= 50: return "Mangelhaft";
+			default: return "Ungenügend";
+		}
+	}
+
+	/// <summary>
+	/// Ziel -> Quelle
+	/// TextBlock -> Slider
+	/// Text -> Value
+	/// string -> double
+	/// 
+	/// (wird hier nicht benötigt)
+	/// </summary>
+	public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+	{
+		throw new NotImplementedException();
+	}
+}
