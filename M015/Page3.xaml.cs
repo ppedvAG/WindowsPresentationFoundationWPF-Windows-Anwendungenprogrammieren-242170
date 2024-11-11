30cda724bf54e0ab4037aa3e797deb5008b8aeaf
ppedvLukasK <lukask@ppedv.de>
2024-11-11 08:04:46 +0100

    M015

---
+++ M015/Page3.xaml.cs
@@ -0,0 +1,17 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace M015
+{
+	public partial class Page3 : UserControl
+	{
+		public Page3() => InitializeComponent();
+	}
+}
